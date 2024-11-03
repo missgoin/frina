dbf102bd24cfafbf0251d23842d594d3999e1967
fredslbn <fh.silaban@gmail.com>
2024-11-03 18:49:55 +0700

    fix error

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,20 +5,3 @@ plugins {
     alias(libs.plugins.kotlin.compose) apply false
 }
 
-fun String.execute(currentWorkingDir: File = file("./")): String {
-    val byteOut = java.io.ByteArrayOutputStream()
-    project.exec {
-        workingDir = currentWorkingDir
-        commandLine = split("\\s".toRegex())
-        standardOutput = byteOut
-    }
-    return String(byteOut.toByteArray()).trim()
-}
-
-val gitCommitCount = "git rev-list HEAD --count".execute().toInt()
-val gitCommitHash = "git rev-parse --verify --short HEAD".execute()
-
-val minSdkVer by extra(24)
-val targetSdkVer by extra(34)
-val buildToolsVer by extra("34.0.0")
-
