8b41059d9ec1ac57d0b8346b2d0f60a3c9bbbe4b
fredslbn <fh.silaban@gmail.com>
2024-11-15 19:04:01 +0700

    try again

--- app/src/main/java/frena/id/service/TimerForegroundService.kt
+++ app/src/main/java/frena/id/service/TimerForegroundService.kt
@@ -9,7 +9,8 @@ import frena.id.service.Constants.ACTION_FOREGROUND_TIMER_BROADCAST
 import frena.id.service.Constants.EXTRA_TIMER
 import frena.id.service.Constants.timeSeconds
 import frena.id.xposed.utils.NotificationUtils
-import frena.id.xposed.service.GeneralCalculations
+import frena.id.service.GeneralCalculations
+import frena.id.service.Constants
 import java.util.*
 
 class TimerForegroundService : Service() {
