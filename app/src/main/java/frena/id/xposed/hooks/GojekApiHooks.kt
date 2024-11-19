0c921f8257c2a186db0eaf06c8654fb44664a6b2
fredslbn <fh.silaban@gmail.com>
2024-11-19 17:24:07 +0700

    try again

--- app/src/main/java/frena/id/xposed/hooks/GojekApiHooks.kt
+++ app/src/main/java/frena/id/xposed/hooks/GojekApiHooks.kt
@@ -12,7 +12,7 @@ import de.robv.android.xposed.XposedBridge
 import de.robv.android.xposed.XposedHelpers
 import de.robv.android.xposed.callbacks.XC_LoadPackage.LoadPackageParam
 
-class GojekApiHooks(XC_LoadPackage.LoadPackageParam) {
+class GojekApiHooks(lpparam: XC_LoadPackage.LoadPackageParam) {
     private val tag = "[GoAPI]"
     
     fun initHooks() {
