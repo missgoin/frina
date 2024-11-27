fb750a6c096272bbcfde338f4142301fdba807c5
fredslbn <fh.silaban@gmail.com>
2024-11-27 14:24:21 +0700

    try again

--- app/src/main/java/frena/id/xposed/utils/GojekUtil.kt
+++ app/src/main/java/frena/id/xposed/utils/GojekUtil.kt
@@ -44,8 +44,8 @@ object GojekUtil {
                     val result: Int = versionCode
                     //val versiGopartner = result
                     
-                    if (versiGopartner): Int {
-                        is Int-> versionCode
+                    if (versiGopartner) {
+                        return versionCode : Int
                     }
                     
                     XposedBridge.log("$tag: version code $versiGopartner")
