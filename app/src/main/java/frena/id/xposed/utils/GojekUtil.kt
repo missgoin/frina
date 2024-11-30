29dfe4905e6e05421febb04dcade5c21338bc3b0
fredslbn <fh.silaban@gmail.com>
2024-11-30 18:27:32 +0700

    try again

--- app/src/main/java/frena/id/xposed/utils/GojekUtil.kt
+++ app/src/main/java/frena/id/xposed/utils/GojekUtil.kt
@@ -43,7 +43,7 @@ object GojekUtil {
                 val versionCode = XposedHelpers.getIntField(pkg, "mVersionCode")
                 val VersiGopartner : Int = versionCode
                 
-                versiGopartner.VersiGopartner = println(VersiGopartner)
+                versiGopartner : Int = println(VersiGopartner)
                 
                     XposedBridge.log("$tag: gp version code $versiGopartner")
             }
