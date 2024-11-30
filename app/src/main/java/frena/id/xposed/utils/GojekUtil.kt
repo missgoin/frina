0679402db1d6d6d3eff1d3c0c5a105986b922af6
fredslbn <fh.silaban@gmail.com>
2024-11-30 19:32:25 +0700

    try again

--- app/src/main/java/frena/id/xposed/utils/GojekUtil.kt
+++ app/src/main/java/frena/id/xposed/utils/GojekUtil.kt
@@ -26,7 +26,7 @@ object GojekUtil {
     private const val tag = "[FRina Util]"
   //  private const val versiGopartner
   //  var versiGopartner : Int = 4186
-    var versiGopartner : Int = 0
+  //  var versiGopartner : Int = 0
     
     @Synchronized
     fun gojekVersionCode(lpparam: XC_LoadPackage.LoadPackageParam): Int {
@@ -43,7 +43,7 @@ object GojekUtil {
                 val versionCode = XposedHelpers.getIntField(pkg, "mVersionCode")
                 val VersiGopartner: Int = versionCode
                 
-                versiGopartner = println("${VersiGopartner}") as Int
+                println("${VersiGopartner}") as Int
                 
                 
                     XposedBridge.log("$tag: gp version code $versiGopartner")
@@ -51,7 +51,7 @@ object GojekUtil {
         } catch (e: Throwable) {
             XposedBridge.log("$tag: error finding version code")
             }
-        return versiGopartner
+        return
     }
     
      
