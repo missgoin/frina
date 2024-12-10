948fa1ec49121ad89d4297675f3f852123dad788
missgoin <mister.goindo@gmail.com>
2024-12-10 11:46:08 +0700

    go ahead fred

--- app/src/main/java/frena/id/manager/MainActivity.kt
+++ app/src/main/java/frena/id/manager/MainActivity.kt
@@ -74,20 +74,6 @@ class MainActivity : ComponentActivity() {
             }
         }
         
-        setContentView(R.layout.activity_main)
-        
-        findViewById<Button>(R.id.start).setOnClickListener {
-            Intent(applicationContext, LocationService::class.java).apply {
-                action = LocationService.ACTION_START
-                startService(this)
-            }
-        }
-        findViewById<Button>(R.id.stop).setOnClickListener {
-            Intent(applicationContext, LocationService::class.java).apply {
-                action = LocationService.ACTION_STOP
-                startService(this)
-            }
-        }        
 
         
     }
